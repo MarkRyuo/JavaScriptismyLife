f8c8888f231edce36b9c1bd8a3a4306670d17a86
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-13 10:19:19 +0800

    update herE

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Container 
 
 const Container = () => {
-    const container = document.getElementById("container").innerHTML = `<input type="text" id="username"> <br> <br> <button type="button" onclick="btndone">Done</button>
+    const container = document.getElementById("container").innerHTML = `<input type="text" id="username"> <br> <br> <button type="button" onclick="btndone()">Done</button>
     ` ;
 
 }
@@ -14,14 +14,15 @@ user = {
     age : 21
 }
 
-let username;
 
-if(username == user.username) {
-    alert("Hello")
-}
-
-const Button_Done = () => {
+const btndone = () => {
     const username = document.getElementById("username").value ;
+
+    if(username == user.username){
+        alert("Lol")
+    }
+
+    
 }
 
 
