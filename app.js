495300c907c00e181e1b8becf75d933e3775296f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-04-13 10:08:16 +0800

    updat eher

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // Container 
 
 const Container = () => {
-    const container = document.getElementById("container").innerHTML = `<input type="text"> <br> ` ;
+    const container = document.getElementById("container").innerHTML = `<input type="text"> <br> <button type="button" onclick="btndone">Done</button>
+    ` ;
 }
 
 Container()
